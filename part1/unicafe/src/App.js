b60a1003af758023f1a4692ee6a41c9170582f9a
Ekene <muomaekene@yahoo.com>
2021-06-09 01:36:41 +0100

    unicafe step2 complete

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -28,6 +28,9 @@ const App = () => {
         <p>Good: {good}</p>
         <p>Neutral: {neutral}</p>
         <p>Bad: {bad}</p>
+        <p>All: {good + neutral + bad}</p>
+        <p>Average: {(good + 0 - 1) / (good + neutral + bad)}</p>
+        <p>Positive: {(good / (good + neutral + bad)) * 100}% </p>
       </div>
     </div>
   );
