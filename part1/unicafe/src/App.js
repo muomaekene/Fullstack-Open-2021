c54d27acb3ca3ed057a40310ce0138f4bef45a58
Ekene <muomaekene@yahoo.com>
2021-06-11 00:06:03 +0100

    unicafe step4 complete

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -6,27 +6,36 @@ const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
+  const [allFeedback, setAllFeedback] = useState([]);
 
   const handleGood = () => {
     setGood(good + 1);
+    setAllFeedback(allFeedback.concat(1));
   };
   const handleNeutral = () => {
     setNeutral(neutral + 1);
+    setAllFeedback(allFeedback.concat(1));
   };
   const handleBad = () => {
     setBad(bad + 1);
+    setAllFeedback(allFeedback.concat(1));
   };
 
   return (
     <div className="app">
       <div className="feedback">
-        <h1>Give Feedback</h1>
+        <h1>give Feedback</h1>
         <button onClick={handleGood}>good</button>
         <button onClick={handleNeutral}>neutral</button>
         <button onClick={handleBad}>bad</button>
       </div>
       <div className="stats">
-        <Statistics good={good} neutral={neutral} bad={bad} />
+        <Statistics
+          good={good}
+          neutral={neutral}
+          bad={bad}
+          feedback={allFeedback}
+        />
       </div>
     </div>
   );
--- part1/unicafe/src/components/Statistics.js
+++ part1/unicafe/src/components/Statistics.js
@@ -1,15 +1,32 @@
 import React from "react";
 
-const Statistics = ({ good, neutral, bad }) => {
+const Statistics = ({ title, good, neutral, bad, feedback }) => {
+  const averageStats = () => {
+    return (good + 0 - 1) / good + neutral + bad;
+  };
+
+  const positiveStats = () => {
+    return (good / (good + neutral + bad)) * 100;
+  };
+
+  if (feedback.length === 0) {
+    return (
+      <div>
+        <h1>statistics</h1>
+        <p>No feedback given</p>
+      </div>
+    );
+  }
+
   return (
     <div>
-      <h1>Statistics</h1>
+      <h1>statistics</h1>
       <div>good: {good}</div>
       <div>neutral: {neutral}</div>
       <div>bad: {bad}</div>
-      <div>All: {good + neutral + bad}</div>
-      <div>Average: {(good + 0 - 1) / (good + neutral + bad)}</div>
-      <div>Positive: {(good / (good + neutral + bad)) * 100} % </div>
+      <div>all: {feedback.reduce((a, b) => a + b, 0)}</div>
+      <div>average: {averageStats()}</div>
+      <div>positive: {positiveStats()}% </div>
     </div>
   );
 };
